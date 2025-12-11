342f3604de438dd8124ecfdfd287cf83d0e6445b
n33miaz <ncormino@gmail.com>
2025-12-11 14:57:24 -0300

    first commit

---
+++ src/main/java/com/n33miaz/stats/GithubStatsApiApplication.java
@@ -0,0 +1,13 @@
+package com.n33miaz.stats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubStatsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubStatsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/n33miaz/stats/GithubStatsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.n33miaz.stats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubStatsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
